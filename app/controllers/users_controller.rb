ffd4adcffd9cd7b0b9f6f20bd2e446d12ca55689
maron0812 <hi6ch30@gmail.com>
2020-12-21 13:46:46 +0000

    top page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+  root to: 'toppages#index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to:'tasks#index'
-    
-  resources :tasks
+  
+  get 'signup', to: 'users#new'  
+  resources :tasks 
+  resources :users, only: [:show, :new, :create]
 end
